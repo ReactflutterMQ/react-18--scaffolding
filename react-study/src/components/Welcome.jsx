fa973dfbf90de2c4b3a0fdb7134b83dd0120e238
root <1425145041@qq.com>
2025-08-28 17:31:21 +0800

    react项目初始化、样式处理及sass应用

---
+++ react-study/src/components/Welcome.jsx
@@ -0,0 +1,72 @@
+// react 17以后可以不用引入React
+// import React from 'react'
+
+// import './Welcome.css'
+/* import './Welcome.scss'
+
+export default function Welcome() {
+  return (
+    <div className='Welcome'>
+      <div className="box">
+        Welcome
+      </div>
+      <div className="box2">
+        Welcome
+      </div>
+    </div>
+  )
+} */
+
+// import style from './Welcome.module.css'
+/* import style from './Welcome.module.scss'
+
+export default function Welcome() {
+  return (
+    <div className='Welcome'>
+      <div className={style.box}>
+        Welcome
+      </div>
+      <div className={style.box2}>
+        Welcome
+      </div>
+    </div>
+  )
+} */
+
+/* import styled from "styled-components"
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: blue;
+  background: red;
+`;
+
+export default function Welcome() {
+  return (
+    <div>
+      {<Title>我是一个标题</Title>}
+    </div>
+  )
+} */
+
+import './Welcome.css'
+import classnames from 'classnames'
+
+/**
+ * 欢迎组件
+ * 一个简单的React函数组件，用于展示欢迎信息
+ */
+export default function Welcome() {
+  // 定义一个CSS类名字符串，用于标题元素的样式
+  // const myClass = 'box box2'
+  const myClass = classnames({
+    box: true,
+    box2: true
+  })
+  return (
+    <div className='Welcome'>
+      <h2 className={myClass}>这是一个标题</h2>
+    </div>
+  )
+}
