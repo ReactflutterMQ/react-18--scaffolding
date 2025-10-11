b371a73588b0dd17e2b4b175fd60a543a4ef3126
root <1425145041@qq.com>
2025-10-11 21:29:48 +0800

    默认路由展示与重定向路由以及404处理

--- react-study/src/router/index.js
+++ react-study/src/router/index.js
@@ -1,16 +1,16 @@
-import { createBrowserRouter, createHashRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, createHashRouter, createRoutesFromElements, Route, Navigate } from 'react-router-dom';
 import App from '../App';
 import Home from '../views/Home/Home';
 import About from '../views/About/About';
 import Foo from '../views/Foo/Foo';
 import Bar from '../views/Bar/Bar';
-import { redirect } from 'react-router-dom';
 
 // 路由表
 const routes = [
     {
         path: '/',
         element: <App />,
+        // errorElement: <div>404，出错了</div>, //全局配置，所有路由都生效
         children: [
             {
                 path: '/',
@@ -21,12 +21,21 @@ const routes = [
                 element: <About />,
                 children: [
                     {
+                        index: true,
+                        // element: <div>默认展示内容</div> //默认子路由
+                        element: <Navigate to="/about/foo/123" /> //实际上就是重定向，类似于vue的redirect
+                    },
+                    {
                         path: 'foo/:id',
                         element: <Foo />
                     },
                     {
                         path: 'bar',
                         element: <Bar />
+                    },
+                    {
+                        path: '*', //当about路由下的子路由没有匹配到时，展示当前路由404页面
+                        element: <div>about路由下的404</div>
                     }
                 ]
             }
